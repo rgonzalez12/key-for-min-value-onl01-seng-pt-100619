afcf688f29d7ec7b7da00bf71dc6021722c0aa1e
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-18 06:05:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,13 +7,3 @@ def key_for_min_value(name_hash)
  empty_hash
 end
 
-
-smallest_value = 0
- empty_hash = nil
- name_hash.each do |name, value|
-  if smallest_value == 0 || value < smallest_value
-    smallest_value = value
-    empty_hash = name
-  end
- end
- empty_hash
