42128022e2ccc4e3253bbb359379ad3c87910848
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-18 06:06:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
- empty_hash = {} 
+ empty_hash = nil
  name_hash.inject do |name, value|
    
   end
